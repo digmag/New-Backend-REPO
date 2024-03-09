c3c8d883ce58f688f81ce6602e3b53182b9ac1b8
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2024-03-09 22:24:24 +0700

    Работа с частичным совпадением по имени

--- src/main/java/com/example/Backend/UserFunctional/UserRepository.java
+++ src/main/java/com/example/Backend/UserFunctional/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.Backend.UserFunctional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,6 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByLogin(String login);
     Optional<UserEntity> findByLoginAndPassword(String login, String password);
 
-    List<UserEntity> findAllByName(String Name);
+    @Query("SELECT e FROM UserEntity e WHERE e.name LIKE %:name%")
+    List<UserEntity> findAllByName(String name);
 }
