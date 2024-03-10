aa3b51158e406d9b3dff5dcafbbbb0a3820e49bf
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2024-03-10 22:09:11 +0700

    spring security

---
+++ src/main/java/com/example/Backend/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.Backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
